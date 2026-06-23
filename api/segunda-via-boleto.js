8ad01898b243301961a8578ddef5f01f3ae469fc
Carlos Lima <developercarloslima@gmail.com>
2026-06-23 13:45:37 -0300

    delete JS de api

--- api/segunda-via-boleto.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(410).json({
-    message: 'Esta rota foi substituída pelo fluxo em etapas: /api/buscar-associado, /api/listar-veiculos e /api/consultar-boletos.'
-  });
-}
